14b1defa85a34e575d6ab943031d304d0ad64125
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-05-20 09:15:45 +0200

    Add timestamps to terminal format

--- lumber/format/term.lua
+++ lumber/format/term.lua
@@ -7,5 +7,5 @@ local colors = { "36", "31", "33", "32", "35" }
 
 return function(level, input)
 	local color = colors[level.index] or 0
-	return string.format("\x1b[%im% 5s\x1b[0m %s", color, level.name, input:gsub("\n", string.format("\n    \x1b[%im↳\x1b[0m ", color)))
+	return string.format("\x1b[37m%s \x1b[%im% 5s \x1b[0m%s", os.date("%H:%M:%S"), color, level.name, input:gsub("\n", string.format("\n    \x1b[%im↳\x1b[0m ", color)))
 end
