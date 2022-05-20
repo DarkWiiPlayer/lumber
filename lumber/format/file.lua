f57f6ef49a71effba8a83f47a41e8d78e883e8cf
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-05-20 11:11:20 +0200

    Add source location to file logger

--- lumber/format/file.lua
+++ lumber/format/file.lua
@@ -1,8 +1,13 @@
 --- Simple log formatter for file output.
--- Writes messages as `YYYY-MM-DD HH:MM:SS -- [LEVEL] log message`.
+-- Writes messages as `YYYY-MM-DD HH:MM:SS +0000 [LEVEL] file:line -- log message`.
 -- @usage
 -- local log = lumber.new((require 'lumber.format.file'))
 
+local function file_line(x)
+	local info = debug.getinfo(x+1)
+	return info.short_src..":"..info.currentline
+end
+
 return function(level, input)
-	return string.format("%s -- [%s] %s", os.date("%Y-%m-%d %H:%M:%S %z"), level.name, input)
+	return string.format("%s [%s] %s -- %s", os.date("%Y-%m-%d %H:%M:%S %z"), level.name, file_line(3), input)
 end
