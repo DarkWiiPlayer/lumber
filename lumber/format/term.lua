da2a13e06335a788ebd5b1502e514f6c7a48ffe2
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-12-12 16:10:24 +0100

    Fix weird incompatibility with yuescript

--- lumber/format/term.lua
+++ lumber/format/term.lua
@@ -8,5 +8,5 @@ local colors = { "36", "31", "33", "32", "35" }
 return function(level, input, context)
 	local color = colors[level.index] or 0
 	prefix = context and ("["..context .. "] ") or ""
-	return string.format("\x1b[2m%s%s \x1b[%im% 5s \x1b[0m%s", prefix, os.date("%H:%M:%S"), color, level.name, input:gsub("\n", string.format("\n" .. string.rep(" ", #prefix + 13) .. "\x1b[%im↳\x1b[0m ", color)))
+	return string.format("\x1b[2m%s%s \x1b[%im%5s \x1b[0m%s", prefix, os.date("%H:%M:%S"), color, level.name, input:gsub("\n", string.format("\n" .. string.rep(" ", #prefix + 13) .. "\x1b[%im↳\x1b[0m ", color)))
 end
